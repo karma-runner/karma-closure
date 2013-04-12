4ec3f976c5cf70321cbcb05963030780e2d22593
Vojta Jina <vojta.jina@gmail.com>
2013-04-11 17:55:07 -0700

    Use Karma instead of Testacular ;-)

---
+++ test-app/karma.conf.js
@@ -0,0 +1,31 @@
+frameworks = ['jasmine', 'closure'];
+
+files = [
+  // closure base
+  {pattern: 'lib/goog/base.js'},
+
+  // included files - tests
+  {pattern: 'test/*.js'},
+
+  // these are only watched and served
+  {pattern: 'js/*.js', included: false},
+
+  // external deps
+  {pattern: 'lib/goog/deps.js', included: false, served: false}
+];
+
+preprocessors = {
+  'test/*.js': ['closure', 'closure-iit'],
+  'js/*.js': ['closure'],
+  'lib/goog/deps.js': ['closure-deps']
+};
+
+plugins = [
+  'closure-deps-resolver',
+  'karma-jasmine',
+  'karma-chrome-launcher'
+];
+
+browsers = ['Chrome'];
+
+autoWatch = true;
