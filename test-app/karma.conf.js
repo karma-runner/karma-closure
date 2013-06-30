bce141e6feac27efa55f7a8495166f13d26d1778
Vojta Jina <vojta.jina@gmail.com>
2013-06-30 03:05:30 -0700

    tests: update test-app config to 0.9.4 syntax

--- test-app/karma.conf.js
+++ test-app/karma.conf.js
@@ -1,31 +1,24 @@
-frameworks = ['jasmine', 'closure'];
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', 'closure'],
+    files: [
+      // closure base
+      {pattern: 'lib/goog/base.js'},
+      // included files - tests
+      {pattern: 'test/*.js'},
+      // these are only watched and served
+      {pattern: 'js/*.js', included: false},
+      // external deps
+      {pattern: 'lib/goog/deps.js', included: false, served: false}
+    ],
 
-files = [
-  // closure base
-  {pattern: 'lib/goog/base.js'},
+    preprocessors: {
+      'test/*.js': ['closure', 'closure-iit'],
+      'js/*.js': ['closure'],
+      'lib/goog/deps.js': ['closure-deps']
+    },
 
-  // included files - tests
-  {pattern: 'test/*.js'},
-
-  // these are only watched and served
-  {pattern: 'js/*.js', included: false},
-
-  // external deps
-  {pattern: 'lib/goog/deps.js', included: false, served: false}
-];
-
-preprocessors = {
-  'test/*.js': ['closure', 'closure-iit'],
-  'js/*.js': ['closure'],
-  'lib/goog/deps.js': ['closure-deps']
+    browsers: ['Chrome'],
+    autoWatch: true
+  });
 };
-
-plugins = [
-  'closure-deps-resolver',
-  'karma-jasmine',
-  'karma-chrome-launcher'
-];
-
-browsers = ['Chrome'];
-
-autoWatch = true;
